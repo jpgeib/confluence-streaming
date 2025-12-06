0066dca1e35b779d595e8efdf46d23169a95a2af
James Geib <jpgeib2@gmail.com>
2025-12-05 19:15:17 -0500

    implemented Routes for auth pages in App container in client side

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../../pages/Home";
 import Channels from "../../pages/Channels";
+import Register from "../../pages/Register";
+import Login from "../../pages/Login";
+import ForgotPassword from "../../pages/ForgotPassword";
+import ResetPassword from "../../pages/ResetPassword";
 
 import "./style.css";
 
@@ -11,6 +15,10 @@ const App = () => {
             <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route exact path="/channels" element={<Channels />} />
+                <Route exact path="/register" element={<Register />} />
+                <Route exact path="/login" element={<Login />} />
+                <Route exact path="/forgot" element={<ForgotPassword />} />
+                <Route exact path="/reset/:id/:token" element={<ResetPassword />} />
             </Routes>
         </div>
     );
