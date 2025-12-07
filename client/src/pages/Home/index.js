896de70a4f29a13a44f406fb09bf07c3e3d03d4a
James Geib <jpgeib2@gmail.com>
2025-12-06 20:12:34 -0500

    confirmed communication between client and server via test route in Home page of client, beginning client-side implementation of auth logic

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { axiosInstance } from "../../utils/api";
 
 import "./style.css";
 
 const Home = () => {
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axiosInstance.get("/test");
+                console.log(res.data);
+            } catch (error) {
+                console.error(error);
+            };
+        };
+        fetchData();
+    }, []);
+
     return (
         <div>
             <h1>Home Page</h1>
