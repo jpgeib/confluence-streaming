e5ae1320b7949487df9d95ce0f59cf5c059732c4
James Geib <jpgeib2@gmail.com>
2025-12-06 21:12:06 -0500

    implemented initial styling and restructuring to Login page in client side

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -4,16 +4,16 @@ import "./style.css";
 
 const Login = () => {
     return (
-        <div className="login-container">
-            <h1>Login Page</h1>
-            <form className="login-form">
-                <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username" required />
+        <div id="login-container">
+            <h1 id="login-header">Login</h1>
+            <form id="login-form">
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" required />
 
                 <label htmlFor="password">Password:</label>
                 <input type="password" id="password" name="password" required />
 
-                <button type="submit">Login</button>
+                <button id="login-btn" type="submit">Login</button>
             </form>
         </div>
     );
