648109f3f91e4147a300f9ddf5a00c3fd307386e
James Geib <jpgeib2@gmail.com>
2025-12-06 21:46:08 -0500

    added styling and restructuring to Register page in client side

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -4,11 +4,14 @@ import "./style.css";
 
 const Register = () => {
     return (
-        <div className="register-container">
-            <h1>Register Page</h1>
-            <form className="register-form">
-                <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username" required />
+        <div id="register-container">
+            <h1 id="register-header">Register Page</h1>
+            <form id="register-form">
+                <label htmlFor="first_name">First Name:</label>
+                <input type="text" id="first_name" name="first_name" required />
+
+                <label htmlFor="last_name">Last Name:</label>
+                <input type="text" id="last_name" name="last_name" required />
 
                 <label htmlFor="email">Email:</label>
                 <input type="email" id="email" name="email" required />
