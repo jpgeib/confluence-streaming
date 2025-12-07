80651d66fe5d4598445d6d53e6cccc4e660a1f80
James Geib <jpgeib2@gmail.com>
2025-12-06 22:05:05 -0500

    restructured and implemented initial styling for ResetPassword page in client side

--- client/src/pages/ResetPassword/index.js
+++ client/src/pages/ResetPassword/index.js
@@ -4,16 +4,16 @@ import "./style.css";
 
 const ResetPassword = () => {
     return (
-        <div className="reset-password-container">
-            <h1>Reset Password Page</h1>
-            <form className="reset-password-form">
-                <label htmlFor="new-password">New Password:</label>
-                <input type="password" id="new-password" name="new-password" required />
+        <div id="reset-container">
+            <h1 id="reset-header">Reset Password Page</h1>
+            <form id="reset-form">
+                <label htmlFor="new_password">New Password:</label>
+                <input type="password" id="new-password" name="new_password" required />
 
-                <label htmlFor="confirm-password">Confirm Password:</label>
-                <input type="password" id="confirm-password" name="confirm-password" required />
+                <label htmlFor="confirm_password">Confirm Password:</label>
+                <input type="password" id="confirm-password" name="confirm_password" required />
 
-                <button type="submit">Reset Password</button>
+                <button id="reset-password-btn" type="submit">Reset Password</button>
             </form>
         </div>
     );
